a49312091efe4f1416ae04d40391c17fd6e7b252
BeholderGit <beholderer@gmail.com>
2022-09-05 20:27:24 +0200

    update description page

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-const app = document.getElementById('root')
+const app = document.getElementById('main')
 
-const logo = document.createElement('img')
-logo.setAttribute('class', 'logo')
-logo.src = 'src/logo.png'
+const logo = document.createElement('a')
+logo.setAttribute('href', './');
+const image = document.createElement('img');
+image.setAttribute('class', 'logo');
+image.setAttribute('src', 'src/logo.png');
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-app.appendChild(logo)
-app.appendChild(container)
+app.appendChild(logo);
+logo.appendChild(image);
+app.appendChild(container);
 
 const URL = "https://ghibliapi.herokuapp.com/films";
 
-
+//Generate home page with all the movies (cards)
 async function fetchTitles() {
     const data = await fetch(URL).then(res => res.json());
     data.forEach(createCard);
 }
-
+//Generate movie page with description and infos
 async function fetchTitleById(id){
     const rep = await fetch(`${URL}/${id}`);
     const response = await rep.json();
-    description(response)
+    description(response);
 }
+
+async function fetchPeopleById(id) {
+    const rep = await fetch(`https://ghibliapi.herokuapp.com/people/${id}`);
+    const response = await rep.json();
+    console.log(response.name)
+}
+
 function createCard(data) {
+    //create link with movie id
     const link = document.createElement('a');
     link.setAttribute('href', "?id="+data.id);
-    //link.insertAdjacentText('beforeend', data.title);
-
-    const card = document.createElement('div')
-    card.setAttribute('class', 'card')
-    card.setAttribute('style', `background-image: url(${data.image})`);
-    
-    container.appendChild(link)
-    link.appendChild(card)
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    card.setAttribute('style', `background-image: url(${data.image})`);   
+    container.appendChild(link);
+    link.appendChild(card);
 } 
 
 function description(response) {
@@ -44,8 +52,26 @@ function description(response) {
 
     container.appendChild(description)
     description.appendChild(text)
+    people(response)
 }
-
+function people(response) {
+    const people = document.createElement('div');
+    const list = document.createElement('ul')
+    people.setAttribute('class', 'people');
+    container.appendChild(people)
+    people.appendChild(list)
+    response.people.forEach(function(element) {
+        var peopleId = element.replace('https://ghibliapi.herokuapp.com/people/','');
+        console.log(peopleId);
+        fetchPeopleById(peopleId);
+        const listA = document.createElement('li');
+        const link = document.createElement('a');
+        link.setAttribute('href', "?people="+peopleId);
+        link.insertAdjacentText('beforeend', response)
+        list.appendChild(listA)
+        listA.appendChild(link)
+    })
+}
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
