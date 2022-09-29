b68dfc2221f6086c86771f928c5c814b0a1b0a4a
BeholderGit <beholderer@gmail.com>
2022-09-29 16:59:32 +0200

    updated with prettier format

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const app = document.getElementById('main')
+const app = document.getElementById("main");
+const logo = document.createElement("a");
+logo.setAttribute("href", "./");
+const image = document.createElement("img");
+image.setAttribute("class", "logo");
+image.setAttribute("src", "src/logo.png");
 
-const logo = document.createElement('a')
-logo.setAttribute('href', './');
-const image = document.createElement('img');
-image.setAttribute('class', 'logo');
-image.setAttribute('src', 'src/logo.png');
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+const container = document.createElement("div");
+container.setAttribute("class", "container");
 
 app.appendChild(logo);
 logo.appendChild(image);
@@ -17,11 +16,11 @@ const URL = "https://ghibliapi.herokuapp.com/films";
 
 //Generate home page with all the movies (cards)
 async function fetchTitles() {
-    const data = await fetch(URL).then(res => res.json());
+    const data = await fetch(URL).then((res) => res.json());
     data.forEach(card);
 }
 //Generate movie page with description and infos
-async function fetchTitleById(id){
+async function fetchTitleById(id) {
     const rep = await fetch(`${URL}/${id}`);
     const response = await rep.json();
     description(response);
@@ -34,65 +33,68 @@ async function fetchPeopleById(id) {
 
 function card(data) {
     //create link with movie id
-    const link = document.createElement('a');
-    link.setAttribute('href', "?id="+data.id);
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('style', `background-image: url(${data.image})`);   
+    const link = document.createElement("a");
+    link.setAttribute("href", "?id=" + data.id);
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("style", `background-image: url(${data.image})`);
     container.appendChild(link);
     link.appendChild(card);
-} 
+}
 
 function description(response) {
-    const title = document.createElement('div');
-    title.setAttribute('class', 'title');
-    const textTitle = document.createElement('p')
-    textTitle.insertAdjacentText('beforeend', response.title)
+    const title = document.createElement("div");
+    title.setAttribute("class", "title");
+    const textTitle = document.createElement("p");
+    textTitle.insertAdjacentText("beforeend", response.title);
 
-    container.appendChild(title)
-    title.appendChild(textTitle)
-    const card = document.createElement('div');
-    card.setAttribute('class', 'img');
-    card.setAttribute('style', `background-image: url(${response.image})`); 
-    container.appendChild(card)
-    const description = document.createElement('div');
-    description.setAttribute('class', 'description');
-    const text = document.createElement('p')
-    text.insertAdjacentText('beforeend', response.description)
+    container.appendChild(title);
+    title.appendChild(textTitle);
+    const card = document.createElement("div");
+    card.setAttribute("class", "img");
+    card.setAttribute("style", `background-image: url(${response.image})`);
+    container.appendChild(card);
+    const description = document.createElement("div");
+    description.setAttribute("class", "description");
+    const text = document.createElement("p");
+    text.insertAdjacentText("beforeend", response.description);
 
-    container.appendChild(description)
-    description.appendChild(text)
-    people(response)
+    container.appendChild(description);
+    description.appendChild(text);
+    people(response);
 }
 function people(response) {
-    const people = document.createElement('div');
-    const list = document.createElement('ul')
-    people.setAttribute('class', 'people');
-    container.appendChild(people)
-    people.appendChild(list)
-    response.people.forEach(function(element) {
-        var peopleId = element.replace('https://ghibliapi.herokuapp.com/people/','');
-        const listA = document.createElement('li');
-        const link = document.createElement('a');
-        if(peopleId){
-            link.setAttribute('href', "?people="+peopleId);
-            fetch(`https://ghibliapi.herokuapp.com/people/${peopleId}`).then(rep => rep.json()).then(data => link.insertAdjacentText('beforeend', data.name))
+    const people = document.createElement("div");
+    const list = document.createElement("ul");
+    people.setAttribute("class", "people");
+    container.appendChild(people);
+    people.appendChild(list);
+    response.people.forEach(function (element) {
+        var peopleId = element.replace(
+            "https://ghibliapi.herokuapp.com/people/",
+            ""
+        );
+        const listA = document.createElement("li");
+        const link = document.createElement("a");
+        if (peopleId) {
+            link.setAttribute("href", "?people=" + peopleId);
+            fetch(`https://ghibliapi.herokuapp.com/people/${peopleId}`)
+                .then((rep) => rep.json())
+                .then((data) =>
+                    link.insertAdjacentText("beforeend", data.name)
+                );
             fetchPeopleById(peopleId);
-            list.appendChild(listA)
-            listA.appendChild(link)
-            }
-    })
+            list.appendChild(listA);
+            listA.appendChild(link);
+        }
+    });
 }
 
-
-
-
-
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const id = params.get("id");
 
 if (id) {
-    fetchTitleById(id)
+    fetchTitleById(id);
 } else {
-    fetchTitles()
+    fetchTitles();
 }
--- script_test.js
+++ script_test.js
@@ -1,13 +1,13 @@
-const app = document.getElementById('main')
+const app = document.getElementById("main");
 
-const logo = document.createElement('a')
-logo.setAttribute('href', './');
-const image = document.createElement('img');
-image.setAttribute('class', 'logo');
-image.setAttribute('src', 'src/logo.png');
+const logo = document.createElement("a");
+logo.setAttribute("href", "./");
+const image = document.createElement("img");
+image.setAttribute("class", "logo");
+image.setAttribute("src", "src/logo.png");
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+const container = document.createElement("div");
+container.setAttribute("class", "container");
 
 app.appendChild(logo);
 logo.appendChild(image);
@@ -16,73 +16,76 @@ app.appendChild(container);
 const URL = "https://ghibliapi.herokuapp.com";
 
 async function fetchMovies() {
-    const rep = await fetch(`${URL}/films`).then(rep => rep.json());
+    const rep = await fetch(`${URL}/films`).then((rep) => rep.json());
     rep.forEach(card);
 }
 function card(movie) {
-    const link = document.createElement('a');
-    link.setAttribute('href', "?id="+movie.id);
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('style', `background-image: url(${movie.image})`);   
+    const link = document.createElement("a");
+    link.setAttribute("href", "?id=" + movie.id);
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("style", `background-image: url(${movie.image})`);
     container.appendChild(link);
     link.appendChild(card);
 }
 
-async function fetchMoviesById(id){
+async function fetchMoviesById(id) {
     const rep = await (await fetch(`${URL}/films/${id}`)).json();
     const peoples = rep.people;
-    const title = document.createElement('h1');
-    title.insertAdjacentText('beforeend', rep.title);
+    const title = document.createElement("h1");
+    title.insertAdjacentText("beforeend", rep.title);
     container.appendChild(title);
-    const original_title = document.createElement('h2');
-    original_title.insertAdjacentText('beforeend', rep.original_title + "("+rep.original_title_romanised+")");
+    const original_title = document.createElement("h2");
+    original_title.insertAdjacentText(
+        "beforeend",
+        rep.original_title + "(" + rep.original_title_romanised + ")"
+    );
     container.appendChild(original_title);
-    const description = document.createElement('p');
-    description.insertAdjacentText('beforeend', rep.description);
+    const description = document.createElement("p");
+    description.insertAdjacentText("beforeend", rep.description);
     container.appendChild(description);
     console.log(rep);
     console.log(rep.release_date);
     console.log(rep.running_time);
     console.log(rep.producer);
     console.log(rep.director);
-    if (peoples == 'https://ghibliapi.herokuapp.com/people/') {
-        const data = false
-        return data
+    if (peoples == "https://ghibliapi.herokuapp.com/people/") {
+        const data = false;
+        return data;
     } else {
-    const data = Promise.all(
-        peoples.map(async (i) => await ( await fetch(i)).json())
-    )
-    return data
+        const data = Promise.all(
+            peoples.map(async (i) => await (await fetch(i)).json())
+        );
+        return data;
     }
 }
 
 function description(data) {
-    if (data != false){
-        const people = document.createElement('div');
-        const list = document.createElement('ul')
-        const title = document.createElement('h3')
-        title.insertAdjacentText('beforeend', "Peoples :")
-        people.setAttribute('class', 'people');
-        container.appendChild(people)
-        people.appendChild(title)
-        people.appendChild(list)
-        data.forEach(i => {
-            const listA = document.createElement('li');
-            const link = document.createElement('a');
-            link.setAttribute('href', "?people="+i.id);
-            link.insertAdjacentText('beforeend', i.name)
-            list.appendChild(listA)
-            listA.appendChild(link)
-    });}
+    if (data != false) {
+        const people = document.createElement("div");
+        const list = document.createElement("ul");
+        const title = document.createElement("h3");
+        title.insertAdjacentText("beforeend", "Peoples :");
+        people.setAttribute("class", "people");
+        container.appendChild(people);
+        people.appendChild(title);
+        people.appendChild(list);
+        data.forEach((i) => {
+            const listA = document.createElement("li");
+            const link = document.createElement("a");
+            link.setAttribute("href", "?people=" + i.id);
+            link.insertAdjacentText("beforeend", i.name);
+            list.appendChild(listA);
+            listA.appendChild(link);
+        });
+    }
 }
 
-
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const id = params.get("id");
 
 if (id) {
-    fetchMoviesById(id).then(data => description(data))
+    fetchMoviesById(id).then((data) => description(data));
 } else {
     fetchMovies();
 }
